729da5f3e557d0692f61b587f8a911a2c809b6cf
esulkey <59899762+esulkey@users.noreply.github.com>
2024-04-03 14:29:40 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,60 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video=document.querySelector("#player1")
+	video.autoplay=false;
+	video.loop=false;
+	console.log ('auto play is set to '+ video.autoplay)
+	console.log('loop is set to ' + video.loop)
+
+	document.querySelector("#play").addEventListener("click", function() {
+        console.log("Play Video");
+        video.play();
+	});
+	document.querySelector("#pause").addEventListener("click", function() {
+		console.log("Pause Video");
+		video.pause();
+	});
+	document.querySelector("#slower").addEventListener("click", function() {
+		console.log("Slow Down");
+		video.playbackRate -= 0.1;
+		console.log("New Speed: " + video.playbackRate);
+	});
+	document.querySelector("#faster").addEventListener("click", function() {
+        console.log("Speed Up");
+        video.playbackRate += 0.1;
+        console.log("New Speed: " + video.playbackRate);
+    });
+	document.querySelector("#skip").addEventListener("click", function() {
+        console.log("Skip Ahead");
+        video.currentTime += 10;
+        if (video.currentTime > video.duration) {
+            video.currentTime = video.duration;
+        }
+        console.log("Current Time: " + video.currentTime);
+    });
+	document.querySelector("#mute").addEventListener("click", function() {
+        if (video.muted) {
+            video.muted = false;
+            console.log("Unmuted");
+        } else {
+            video.muted = true;
+            console.log("Muted");
+        }
+    });
+	document.querySelector("#slider").addEventListener("input", function() {
+        video.volume = this.value / 100;
+        document.querySelector("#volume").textContent = this.value;
+		console.log("Volume: " + video.volume * 100);
+    });
+	document.querySelector("#vintage").addEventListener("click", function() {
+        console.log("Switching to Old School");
+        video.classList.add("oldSchool");
+    });
+
+    document.querySelector("#orig").addEventListener("click", function() {
+        console.log("Switching to Original");
+        video.classList.remove("oldSchool");
+    });
+});
